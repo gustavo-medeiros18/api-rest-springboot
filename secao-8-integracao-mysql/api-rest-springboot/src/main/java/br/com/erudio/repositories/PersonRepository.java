f0c04effa4398d85b7b18b9f3a4c95bbf6a1da4e
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-16 14:24:20 -0300

    :sparkles: feat: Adds Repository annotation.

--- secao-8-integracao-mysql/api-rest-springboot/src/main/java/br/com/erudio/repositories/PersonRepository.java
+++ secao-8-integracao-mysql/api-rest-springboot/src/main/java/br/com/erudio/repositories/PersonRepository.java
@@ -2,7 +2,9 @@ package br.com.erudio.repositories;
 
 import br.com.erudio.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
