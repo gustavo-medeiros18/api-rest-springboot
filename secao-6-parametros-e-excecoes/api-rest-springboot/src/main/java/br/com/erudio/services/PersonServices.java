235d0da308d85bc546fecad80cd71017a213f801
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-16 09:27:57 -0300

    :sparkles: feat: Adds findAll() and mockPerson() methods in PersonServices.

--- secao-6-parametros-e-excecoes/api-rest-springboot/src/main/java/br/com/erudio/services/PersonServices.java
+++ secao-6-parametros-e-excecoes/api-rest-springboot/src/main/java/br/com/erudio/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio.services;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,18 @@ public class PersonServices {
       PersonServices.class.getName()
   );
 
+  public List<Person> findAll() {
+    logger.info("Finding all people!");
+
+    List<Person> persons = new ArrayList<>();
+    for (int i = 0; i < 8; i++) {
+      Person person = mockPerson(i);
+      persons.add(person);
+    }
+
+    return persons;
+  }
+
   public Person findById(String id) {
     logger.info("Finding one person!");
 
@@ -22,6 +36,18 @@ public class PersonServices {
     person.setFirstName("Gustavo");
     person.setLastName("Medeiros");
     person.setAddress("Sobral - Ceara - Brasil");
+    person.setGender("Male");
+
+    return person;
+  }
+
+  private Person mockPerson(int i) {
+    Person person = new Person();
+
+    person.setId(counter.incrementAndGet());
+    person.setFirstName("Person Name " + counter.get());
+    person.setLastName("Last Name " + counter.get());
+    person.setAddress("Some Address in Brazil " + counter.get());
     person.setGender("Male");
 
     return person;
