137a9b1e3041eb5b6eae1029fd8addeecf81354c
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-16 10:49:21 -0300

    :sparkles: feat: Adds secao-8 project initial files.

---
+++ secao-8-integracao-mysql/api-rest-springboot/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,71 @@
+package br.com.erudio.services;
+
+import br.com.erudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+  private final AtomicLong counter = new AtomicLong();
+  private Logger logger = Logger.getLogger(
+      PersonServices.class.getName()
+  );
+
+  public List<Person> findAll() {
+    logger.info("Finding all people!");
+
+    List<Person> persons = new ArrayList<>();
+    for (int i = 0; i < 8; i++) {
+      Person person = mockPerson(i);
+      persons.add(person);
+    }
+
+    return persons;
+  }
+
+  public Person findById(String id) {
+    logger.info("Finding one person!");
+
+    Person person = new Person();
+
+    person.setId(counter.incrementAndGet());
+    person.setFirstName("Gustavo");
+    person.setLastName("Medeiros");
+    person.setAddress("Sobral - Ceara - Brasil");
+    person.setGender("Male");
+
+    return person;
+  }
+
+  public Person createPerson(Person person) {
+    logger.info("Creating one person!");
+
+    return person;
+  }
+
+  public Person updatePerson(Person person) {
+    logger.info("Updating one person!");
+
+    return person;
+  }
+
+  public void delete(String id) {
+    logger.info("Deleting one person!");
+  }
+
+  private Person mockPerson(int i) {
+    Person person = new Person();
+
+    person.setId(counter.incrementAndGet());
+    person.setFirstName("Person Name " + counter.get());
+    person.setLastName("Last Name " + counter.get());
+    person.setAddress("Some Address in Brazil " + counter.get());
+    person.setGender("Male");
+
+    return person;
+  }
+}
