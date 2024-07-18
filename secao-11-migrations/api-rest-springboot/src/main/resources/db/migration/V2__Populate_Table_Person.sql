d39b7032de5d7eea81ef8a384004b6347deac472
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-18 10:43:09 -0300

    🗃️ feat: Adds Person-related migrations.

---
+++ secao-11-migrations/api-rest-springboot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (id, address, first_name, gender, last_name) VALUES
+                                                                      (1, 'Sobral - Ceara - Brasil', 'Gustavo', 'Male', 'Medeiros'),
+                                                                      (4, 'Sobral - Ceara - Brasil', 'Gustavo', 'Male', 'Medeiros'),
+                                                                      (5, 'Sobral - Ceara - Brasil', 'Gustavo', 'Male', 'Medeiros'),
+                                                                      (6, 'Sobral - Ceara - Brasil', 'Gustavo', 'Male', 'Medeiros'),
+                                                                      (8, 'Sobral - Ceara - Brasil', 'Gustavo', 'Male', 'Medeiros'),
+                                                                      (9, 'Sobral - Ceara - Brasil', 'James', 'Male', 'Bond');
