48e5eaabd66f2aab58da76d84e052c2451b01526
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-19 10:13:49 -0300

    :sparkles: feat: Adds MediaType util class.

---
+++ secao-13-content-negotiation/api-rest-springboot/src/main/java/br/com/erudio/util/MediaType.java
@@ -0,0 +1,7 @@
+package br.com.erudio.util;
+
+public class MediaType {
+  public static final String APLLICATION_JSON = "application/json";
+  public static final String APLLICATION_XML = "application/xml";
+  public static final String APPLICATION_YML = "application/x-yaml";
+}
