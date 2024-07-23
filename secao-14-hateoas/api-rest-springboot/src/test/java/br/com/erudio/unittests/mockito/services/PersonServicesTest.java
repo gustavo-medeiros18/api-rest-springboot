8a011ef9036b18a07c5a3907bf04d96990842ed6
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-23 11:19:43 -0300

    ✅ test: Adds PersonServicesTest unit test class.

---
+++ secao-14-hateoas/api-rest-springboot/src/test/java/br/com/erudio/unittests/mockito/services/PersonServicesTest.java
@@ -0,0 +1,31 @@
+package br.com.erudio.unittests.mockito.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonServicesTest {
+
+  @BeforeEach
+  void setUp() throws Exception{
+  }
+
+  @Test
+  void testFindAll() {
+  }
+
+  @Test
+  void testFindById() {
+  }
+
+  @Test
+  void testCreate() {
+  }
+
+  @Test
+  void testUpdate() {
+  }
+
+  @Test
+  void testDelete() {
+  }
+}
