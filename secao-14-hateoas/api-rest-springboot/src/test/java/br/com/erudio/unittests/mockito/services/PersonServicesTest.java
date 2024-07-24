3557141fca32344df9ea3139aaf6fd664f8ec9f2
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-24 10:19:09 -0300

    ✅ test: Adds unit test for PersonServices.update() method.

--- secao-14-hateoas/api-rest-springboot/src/test/java/br/com/erudio/unittests/mockito/services/PersonServicesTest.java
+++ secao-14-hateoas/api-rest-springboot/src/test/java/br/com/erudio/unittests/mockito/services/PersonServicesTest.java
@@ -147,6 +147,30 @@ class PersonServicesTest {
 
   @Test
   void testUpdate() {
+    Person entity = input.mockEntity(1);
+    entity.setId(1L);
+
+    Person persisted = entity;
+    persisted.setId(1L);
+
+    PersonVO vo = input.mockVO(1);
+    vo.setKey(1L);
+
+    when(repository.findById(1L)).thenReturn(Optional.of(entity));
+    when(repository.save(entity)).thenReturn(persisted);
+
+    var result = services.update(vo);
+
+    assertNotNull(result);
+    assertNotNull(result.getKey());
+    assertNotNull(result.getLinks());
+
+    assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+
+    assertEquals("Addres Test1", result.getAddress());
+    assertEquals("First Name Test1", result.getFirstName());
+    assertEquals("Last Name Test1", result.getLastName());
+    assertEquals("Female", result.getGender());
   }
 
   @Test
