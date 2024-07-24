1ec2e3ce17ab2f10be51618430d85d3ab080d1f4
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-24 10:54:55 -0300

    :sparkles: feat: Adds RequiredObjectIsNull exception throwing in create() and update() methods.

--- secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/services/PersonServices.java
+++ secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/services/PersonServices.java
@@ -2,6 +2,7 @@ package br.com.erudio.services;
 
 import br.com.erudio.controllers.PersonController;
 import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.exceptions.RequiredObjectIsNullException;
 import br.com.erudio.exceptions.ResourceNotFoundException;
 import br.com.erudio.mapper.DozerMapper;
 import br.com.erudio.model.Person;
@@ -70,6 +71,9 @@ public class PersonServices {
   }
 
   public PersonVO create(PersonVO person) {
+    if (person == null)
+      throw new RequiredObjectIsNullException();
+
     logger.info("Creating one person!");
 
     var entity = DozerMapper.parseObject(person, Person.class);
@@ -85,6 +89,9 @@ public class PersonServices {
   }
 
   public PersonVO update(PersonVO person) {
+    if (person == null)
+      throw new RequiredObjectIsNullException();
+
     logger.info("Updating one person!");
 
     /**
