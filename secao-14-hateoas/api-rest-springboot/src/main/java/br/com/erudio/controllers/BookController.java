11ad725bfc2a9bfb9881634e958cad10cee17848
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-25 10:45:28 -0300

    :sparkles: feat: Updates BookController.findAll() method to use the service.

--- secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/controllers/BookController.java
+++ secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/controllers/BookController.java
@@ -1,17 +1,25 @@
 package br.com.erudio.controllers;
 
+import br.com.erudio.model.Book;
+import br.com.erudio.services.BookServices;
 import br.com.erudio.util.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/book/v1")
 public class BookController {
+  @Autowired
+  private BookServices service;
+
   @GetMapping(
       produces = MediaType.APLLICATION_JSON
   )
-  public String findAll() {
-    return "API REST Spring Boot";
+  public List<Book> findAll() {
+    return service.findAll();
   }
 }
