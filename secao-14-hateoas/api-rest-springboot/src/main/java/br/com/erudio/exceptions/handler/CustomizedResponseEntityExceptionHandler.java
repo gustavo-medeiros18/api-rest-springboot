15b72fa1692917d38160afd906f9a0d9eda319f1
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-06 11:09:39 -0300

    🥅 feat: Adds method to handle InvalidJwtAuthenticationException.

--- secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.erudio.exceptions.handler;
 
 import br.com.erudio.exceptions.ExceptionResponse;
+import br.com.erudio.exceptions.InvalidJwtAuthenticationException;
 import br.com.erudio.exceptions.RequiredObjectIsNullException;
 import br.com.erudio.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -56,5 +57,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     );
 
     return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(InvalidJwtAuthenticationException.class)
+  public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(
+      Exception ex,
+      WebRequest request
+  ) {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false)
+    );
+
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
   }
 }
