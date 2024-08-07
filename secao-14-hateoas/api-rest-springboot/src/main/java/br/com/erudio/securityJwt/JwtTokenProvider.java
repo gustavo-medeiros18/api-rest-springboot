c8c894e820ceb942b8f128be32e1971e3cdd5a35
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-07 10:03:15 -0300

    :memo: docs: Adds explicative comments in JwtTokenProvider class.

--- secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/securityJwt/JwtTokenProvider.java
+++ secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/securityJwt/JwtTokenProvider.java
@@ -14,6 +14,10 @@ import java.util.List;
 
 @Service
 public class JwtTokenProvider {
+  /**
+   * If a value is not provided for the 'security.jwt.token.secret-key'
+   * property, the default value 'secret' is used.
+   */
   @Value("${security.jwt.token.secret-key: secret}")
   private String secretKey = "secret";
 
@@ -23,8 +27,29 @@ public class JwtTokenProvider {
   @Autowired
   private UserDetailsService userDetailsService;
 
+  /**
+   * This field here represents the hashing algorithm that
+   * will be used to create a jwt token based on user data
+   * and the secret key.
+   */
   Algorithm algorithm = null;
 
+  /**
+   * The `init` method is annotated with `@PostConstruct`, which means it will be
+   * executed automatically by the Spring framework after the bean's properties
+   * have been initialized. This method performs the following tasks:
+   * <p>
+   * 1. Encodes the `secretKey` field in Base64 format.
+   * 2. Initializes the `algorithm` field with the HMAC256 algorithm using the
+   * encoded `secretKey`.
+   * <p>
+   * If the PostConstruct annotation is not used, the `init` method would be executed
+   * only when manually called by the developer.
+   * <p>
+   * If this method is not executed, the `secretKey` will not be encoded, and the
+   * `algorithm` field will not be initialized, which will cause any JWT token
+   * creation or validation operations to fail.
+   */
   @PostConstruct
   protected void init() {
     secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
@@ -34,7 +59,7 @@ public class JwtTokenProvider {
   public TokenVO createAccessToken(String username, List<String> roles) {
     Date now = new Date();
     Date validity = new Date(now.getTime() + validityInMilliseconds);
-    
+
     var accessToken = getAccessToken(username, roles, now, validity);
     var refreshToken = getAccessToken(username, roles, now);
 
