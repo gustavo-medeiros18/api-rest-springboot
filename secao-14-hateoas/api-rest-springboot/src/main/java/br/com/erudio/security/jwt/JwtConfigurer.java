c2f8bc9afcb645863ede7f8adcfcbcd5b5acc6fd
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-08 11:09:19 -0300

    📝 docs: Adds descriptive comments in JwtConfigurer class.

--- secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
+++ secao-14-hateoas/api-rest-springboot/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
@@ -7,6 +7,13 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Service;
 
+/**
+ * This class is responsible for configuring the JwtTokenFilter in the Spring Security filter chain.
+ * It needs to extend the SecurityConfigurerAdapter class and override the configure method.
+ * <p>
+ * The SecurityConfigurerAdapter class is a generic class that receives the DefaultSecurityFilterChain
+ * and HttpSecurity as parameters. It serves as a base class for all security configurers in Spring Security.
+ */
 @Service
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
   @Autowired
@@ -16,9 +23,23 @@ public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilt
     this.jwtTokenProvider = jwtTokenProvider;
   }
 
+  /**
+   * The configure method is responsible for adding the JwtTokenFilter to the Spring
+   * Security filter chain.
+   */
   @Override
   public void configure(HttpSecurity http) {
     JwtTokenFilter customFilter = new JwtTokenFilter(jwtTokenProvider);
+
+    /**
+     * The UsernamePasswordAuthenticationFilter is the filter responsible for authenticating the user.
+     * It is the filter that receives the user credentials and authenticates the user.
+     * <p>
+     * The customFilter is the JwtTokenFilter that we created to validate the JWT token.
+     * <p>
+     * Here, we are adding the JwtTokenFilter before the UsernamePasswordAuthenticationFilter.
+     * This way, the JwtTokenFilter will be executed before the UsernamePasswordAuthenticationFilter.
+     */
     http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
   }
 }
